7c29fa3448000da77296f587c4aa5dcf58623599
Anoop <kaur0550@algonquinlive.com>
2022-11-11 18:22:12 -0500

    class10.2 end

--- pages/api/books/[id].js
+++ pages/api/books/[id].js
@@ -1,7 +1,7 @@
 import nc from 'next-connect';
 import cors from 'cors';
 
-import {loadBooks} from '../../../datasource/rw-utility';
+import {loadBooks, saveBooks} from '../../../datasource/rw-utility';
 
 let books = [];
 books = loadBooks();
@@ -20,6 +20,35 @@ const handler = nc()
       }
 
       res.json({book: book});
+  })
+  .patch((req, res) => {
+    const book = getBook(req.query.id)
+    if(!book){
+        res.status(404);
+        res.end();
+        return
+    }
+    const idx = books.findIndex((item) => item.id === req.query.id);
+    const updated = {...req.body}
+
+    books[idx] = updated;
+    saveBooks(books)
+    res.json({book: updated})
+  })
+  .delete((req, res)=>{
+    const book = getBook(req.query.id)
+
+    if(!book){
+        res.status(404);
+        res.end();
+        return
+    }
+    const idx = books.findIndex((item) => item.id === req.query.id);
+
+    books.splice(idx, 1);
+    saveBooks(books);
+    res.json({book: book})
+
   })
 
 
--- pages/books/[id].js
+++ pages/books/[id].js
@@ -5,8 +5,12 @@ import Link from 'next/link'
 export default function Details(){
     const router = useRouter();
     const {id} = router.query;
+    const titleRef = useRef();
+    const authorRef = useRef();
+    const detailsRef = useRef();
 
     const [book, setBook] = useState(null)
+    const [isEditing, setIsEditing] = useState(false)
 
     useEffect(()=>{
         fetch(`/api/books/${id}`, {method: 'GET'})
@@ -21,6 +25,42 @@ export default function Details(){
             console.log(err)
         })
     },[id])
+
+    async function handleDelete(){
+        const res = await fetch(`/api/books/${id}`, {
+            method: 'DELETE'
+        })
+
+        const data = await res.json()
+        console.log(data)
+        router.push('/books')
+    }
+
+    async function handleEdit(){
+        setIsEditing(true)
+    }
+    
+    async function handleSave(){
+        const updatedBook ={
+            id: id,
+            title: titleRef.current.value,
+            author: authorRef.current.value,
+            details: detailsRef.current.value
+        }
+        const res = await fetch(`/api/books/${id}`, {
+            method: 'PATCH',
+            body: JSON.stringify(updatedBook),
+            headers: {
+                'Content-Type' : 'application/json'
+            }
+        })
+        const data = await res.json()
+        console.log(data)
+        setIsEditing(false)
+    }
+
+
+
     if(!book){
         return null
     } else{
@@ -33,11 +73,26 @@ export default function Details(){
         <Link href="/books">Show Collection</Link>
         <Link href="/books/addnew">Add New</Link>
       </nav>
+      {isEditing ? <div>
+        <form>
+            <label>Book Name:</label>
+            <input defaultValue = {book.title} type="text" ref = {titleRef} />
+            <label>Book Author:</label>
+            <input defaultValue = {book.author} type="text" ref = {authorRef} />
+            <label>Book Details:</label>
+            <input defaultValue = {book.details} type="text" ref = {detailsRef} />
+            <button onClick={handleSave}>Save</button>
+            <button >Cancle</button>
+        </form>
+      </div> : 
         <div>
             <h1>{book.title}</h1>
             <h2>{book.author}</h2>
             <p>{book.details}</p>
+            <button onClick={handleEdit}>Edit</button>
+            <button onClick={handleDelete}>Delete</button>
         </div>
+      }
         </>
     )
     }
